0bb0db9d71611723e32ef54fbd9902d8bdf1ffb5
Shogo N <shogonir@gmail.com>
2020-01-07 03:02:14 +0900

    [modify] fix width axis

--- src/map/canvas/layer/XYAxisLayer.ts
+++ src/map/canvas/layer/XYAxisLayer.ts
@@ -18,16 +18,12 @@ export default class XYAxisLayer implements Layer {
   constructor(gl: WebGL2RenderingContext, status: MMapStatus) {
     this.gameObjects = []
 
-    const heightAsUnit = status.maxUnit.y - status.minUnit.y
-    const widthAsUnit = status.maxUnit.x - status.minUnit.x
-    const ptu = CanvasUtils.calculatePixelToUnit(status.zoom)
-
     const planeX = new PlaneGeometry(1.0)
     const blackMaterialX = new SingleColorMaterial(gl, planeX, Color.black())
     this.xAxis = new GameObject(
       Vector3.zero(),
       Quaternion.fromRadianAndVector3(0, new Vector3(0, 1, 0)),
-      new Vector3(widthAsUnit, ptu, 1.0),
+      Vector3.zero(),
       blackMaterialX
     )
 
@@ -36,16 +32,37 @@ export default class XYAxisLayer implements Layer {
     this.yAxis = new GameObject(
       Vector3.zero(),
       Quaternion.fromRadianAndVector3(0, new Vector3(0, 1, 0)),
-      new Vector3(ptu, heightAsUnit, 1.0),
+      Vector3.zero(),
       blackMaterialY
     )
+
+    this.update(status)
 
     this.gameObjects.push(this.xAxis)
     this.gameObjects.push(this.yAxis)
   }
 
   update(status: MMapStatus) {
+    this.updatePosition(status)
+    this.updateScale(status)
+  }
+
+  updatePosition(status: MMapStatus) {
     this.xAxis.position.y = status.mappingY(0)
     this.yAxis.position.x = status.mappingX(0)
+  }
+
+  updateScale(status: MMapStatus) {
+    const ptu = CanvasUtils.calculatePixelToUnit(status.zoom)
+    const heightAsUnit = status.maxUnit.y - status.minUnit.y
+    const widthAsUnit = status.maxUnit.x - status.minUnit.x
+
+    this.xAxis.scale.x = widthAsUnit
+    this.xAxis.scale.y = ptu
+    this.xAxis.scale.z = 1.0
+
+    this.yAxis.scale.x = ptu
+    this.yAxis.scale.y = heightAsUnit
+    this.yAxis.scale.z = 1.0
   }
 }
