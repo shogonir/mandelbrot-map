3e332c63d3d10262a5d857038ef0da81ea517553
Shogo N <shogonir@gmail.com>
2020-01-25 09:35:12 +0900

    [modify] chrome.sh stdout to null

--- script/chrome.sh
+++ script/chrome.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --allow-file-access-from-files &
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+    --allow-file-access-from-files \
+    > /dev/null 2>&1 &
