7594791a4b6ff4cf0d3e5408fce040ff98e837b1
Shogo N <shogonir@gmail.com>
2020-02-23 01:16:14 +0900

    [add] Circle2.contactsToVector2()

--- src/common/Circle2.ts
+++ src/common/Circle2.ts
@@ -1,4 +1,5 @@
 import Vector2 from './Vector2'
+import MathUtil from './MathUtil'
 
 export default class Circle2 {
 
@@ -8,5 +9,27 @@ export default class Circle2 {
   constructor(center: Vector2, radius: number) {
     this.center = center
     this.radius = radius
+  }
+
+  contactsToVector2(p: Vector2): Vector2[] | undefined {
+    const c = this.center
+    const r = this.radius
+
+    const a = - ((p.y - c.y) ** 2) - ((p.x - c.x) ** 2)
+    const b = 2 * c.x * ((p.y - c.y) ** 2 + (p.x - c.x) ** 2) + 2 * r ** 2 * (p.x - c.x)
+    const cc = (p.y - c.y) ** 2 * (r ** 2 - c.x ** 2) - r ** 4 + c.x ** 2 * (p.x - c.x) ** 2 - 2 * r ** 2 * c.x * (p.x - c.x)
+    const mayBeXs: number[] | undefined = MathUtil.solveQuadraticEquation(a, b, cc)
+    
+    if (mayBeXs === undefined) {
+      return undefined
+    }
+    const xs: number[] = mayBeXs
+    
+    const contacts: Vector2[] = []
+    for (const x of xs) {
+      const y = c.y + r ** 2 / ((p.x - c.x) * (p.x - c.x) * (p.y - c.y))
+      contacts.push(new Vector2(x, y))
+    }
+    return contacts
   }
 }
