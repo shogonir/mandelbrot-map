736f3cb5e8849919aaf529707eb70dab1d3b510a
Shogo N <shogonir@gmail.com>
2020-02-24 23:43:49 +0900

    [add] Circle3.contactsToVector3()

--- src/common/Circle3.ts
+++ src/common/Circle3.ts
@@ -1,5 +1,7 @@
 import Vector3 from './Vector3'
 import Plane3 from './Plane3'
+import Circle2 from './Circle2'
+import Vector2 from './Vector2'
 
 export default class Circle3 {
 
@@ -11,5 +13,13 @@ export default class Circle3 {
     this.plane = plane
     this.center = center
     this.radius = radius
+  }
+
+  contactsToVector3(p: Vector3): Vector3[] | undefined {
+    const mayBeContacts = new Circle2(Vector2.zero(), this.radius).contactsToVector2(p.toVector2())
+    if (mayBeContacts === undefined) {
+      return undefined
+    }
+    return []
   }
 }
